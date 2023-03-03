1d469ff907fab39d37982a17000dea8b8d1e0a37
Benjohn007 <johanben330@gmail.com>
2023-03-03 14:06:52 +0100

    Create an Expense Tracker using ASP.Net 6 MVC

---
+++ Expense Tracker/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Expense_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
